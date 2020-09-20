687b7a92ea74bd4453610a29f8892c593464de1b
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-09-20 18:44:33 -0400

    Finished M4L2

--- M4L2 - springbootdemo/src/main/java/sg/springboot/MainController.java
+++ M4L2 - springbootdemo/src/main/java/sg/springboot/MainController.java
@@ -1,8 +1,7 @@
-/*
-http://localhost:8080/test
- */
 package sg.springboot;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,10 +15,10 @@ public class MainController {
     int number = 42;
 
     /**
-     * GET - add obj's to page and get page name
+     * GET at http://localhost:8080/test - add obj's to page and get page name
      *
      * @param model {Model} put data to render to page, add as k/v pairs
-     * @return {String} name of the page
+     * @return {String} subdomain
      */
     @GetMapping("test")
     public String testPage(Model model) {
@@ -30,8 +29,8 @@ public class MainController {
     }
 
     /**
-     * POST - retrieve form inputs and store to variables, force redirect to
-     * /test
+     * POST at http://localhost:8080/test - retrieve form inputs and store to
+     * variables, force redirect to /test
      *
      * @param request {HttpServletRequest} used to pull the form data
      * @return {String} redirect back to base
@@ -42,5 +41,42 @@ public class MainController {
         number = Integer.parseInt(request.getParameter("formNumber"));
 
         return "redirect:/test";
+    }
+
+    /**
+     * GET at http://localhost:8080/testList - create a list and add numbers to
+     * it
+     *
+     * @param model {Model} obj with a list of numbers as a field
+     * @return {String} the subdomain
+     */
+    @GetMapping("testList")
+    public String testList(Model model) {
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.add(0);
+        numbers.add(10);
+        numbers.add(6);
+        numbers.add(35);
+
+        model.addAttribute("numberList", numbers);
+
+        return "testList";
+    }
+
+    /**
+     * GET at http://localhost:8080/testConditional - create a obj for use with
+     * a condition
+     *
+     * @param model {Model} obj with a boolean, int, and string field
+     * @return the subdomain
+     */
+    @GetMapping("testConditional")
+    public String testConditional(Model model) {
+        model.addAttribute("truth", true);
+        model.addAttribute("theNumber", 33);
+        model.addAttribute("aString", "testing");
+
+        return "testConditional";
     }
 }
